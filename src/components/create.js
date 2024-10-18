0207966df947397cc16960e4594d577825709a98
Rebecca Nolan <G00423921@atu.ie>
2024-10-18 11:14:51 +0000

    first commit

--- src/components/create.js
+++ src/components/create.js
@@ -1,8 +1,61 @@
-const Create = ()=>{
-    return(
+import { useState } from "react"; //used to store data return data from API
+
+const Create = () => {
+    //state - current state value
+    //setState - allows state value to update
+    const[title, setTitle] = useState(''); 
+    const[year, setYear] = useState('');
+    const[poster, setPoster] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); //so it's not called multiple times
+        console.log(title); //output titles 
+        console.log(year); //output year 
+        console.log(poster); //output poster 
+    }
+
+    return (
         <div>
-            <h3>Hello from create component!</h3>
+            <h2>Create Component</h2>
+            {/* upload data to a server */}
+            <form onSubmit={handleSubmit}> 
+
+                <div className = "form-group">
+                    <label>add movie title:</label>
+                    <input type="text"
+                    className = "form-control"
+                    value={title}
+                    onChange={(e) => {setTitle(e.target.value)}} //adds
+                    />
+                </div>
+
+                <div className = "form-group">
+                    <label>add year:</label>
+                    <input type="text"
+                    className = "form-control"
+                    value={year}
+                    onChange={(e) => {setYear(e.target.value)}} //adds
+                    />
+                </div>
+
+                <div className = "form-group">
+                    <label>add poster:</label>
+                    <input type="text"
+                    className = "form-control"
+                    value={poster}
+                    onChange={(e) => {setPoster(e.target.value)}} //adds
+                    />
+                </div>
+                <input type="submit" value="add poster"/> 
+                
+            </form>
         </div>
-    );
+    )
+   
 }
 export default Create;
+
+//comments - explain
+//state
+//useeffect
+//axios
